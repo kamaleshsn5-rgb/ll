76ece12b172de3505ae62c24ed56aab2538eab5c
kamaleshsn5-rgb <kamaleshsn5@gmail.com>
2026-09-16 12:14:01 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,49 +1,15 @@
-`default_nettype none
-`timescale 1ns / 1ps
+module tb_half_adder;
+  reg a, b;
+  wire sum, carry;
 
-/* This testbench just instantiates the module and makes some convenient wires
-   that can be driven / tested by the cocotb test.py.
-*/
-module tb ();
+  tt_um_kamalesh_halfadder uut (.a(a), .b(b), .sum(sum), .carry(carry));
 
-  // Dump the signals to a FST file. You can view it with gtkwave or surfer.
   initial begin
-    $dumpfile("tb.fst");
-    $dumpvars(0, tb);
-    #1;
+    $display("A B | SUM CARRY");
+    a=0; b=0; #10 $display("%b %b | %b   %b", a, b, sum, carry);
+    a=0; b=1; #10 $display("%b %b | %b   %b", a, b, sum, carry);
+    a=1; b=0; #10 $display("%b %b | %b   %b", a, b, sum, carry);
+    a=1; b=1; #10 $display("%b %b | %b   %b", a, b, sum, carry);
+    $finish;
   end
-
-  // Wire up the inputs and outputs:
-  reg clk;
-  reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
-`ifdef GL_TEST
-  wire VPWR = 1'b1;
-  wire VGND = 1'b0;
-`endif
-
-  // Replace tt_um_example with your module name:
-   tt_um_kamalesh_halfadder user_project (
-
-      // Include power ports for the Gate Level test:
-`ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
-`endif
-
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
-  );
-
 endmodule
